d37f7129d7427bdbac1086eea1b968b7c4a7c78e
mprecilio <84741853+mprecilio@users.noreply.github.com>
2021-08-18 08:32:51 -0500

    Added bookmarks

---
+++ Christal/PostService/src/main/java/rev/service/BookmarksServiceImpl.java
@@ -0,0 +1,37 @@
+package rev.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import rev.dao.BookmarksDao;
+import rev.model.Bookmarks;
+import rev.model.Post;
+
+@Service
+public class BookmarksServiceImpl implements BookmarksService {
+	
+	private BookmarksDao bookmarksDao;
+	
+	
+
+	@Override
+	@Transactional
+	public Bookmarks bookmarkAPost(Bookmarks bookmark) {
+		boolean alreadybookmarked = bookmarksDao.existsByUserIdAndPost(bookmark.getUserId(), bookmark.getPost());
+		if(alreadybookmarked) {
+			bookmarksDao.deleteAllByUserIdAndPost(bookmark.getUserId(), bookmark.getPost());
+			alreadybookmarked = bookmarksDao.existsByUserIdAndPost(bookmark.getUserId(), bookmark.getPost());
+			return null;
+		}
+			return bookmarksDao.save(bookmark);
+	}
+
+	@Override
+	public List<Bookmarks> findAllBookmarksForPost(Post post) {
+		return bookmarksDao.findByPost(post);
+	}
+
+}
